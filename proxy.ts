03baba5bafd3c365f0456aeee5f01f8bb5a3c8fa
MrSayyedAnas <sayyedanas7172@gmail.com>
2026-04-02 21:50:43 +0530

    fix: remove middleware and add session + update proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-
+import { updateSession } from '@/lib/supabase/session'
 export function proxy(request: NextRequest) {
   return NextResponse.next()
 }
